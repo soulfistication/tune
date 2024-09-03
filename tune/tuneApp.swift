c0913abb33e65e0298cc04b495b6b66f68554204
Ivan Almada <ivan.almada@me.com>
2024-09-02 21:56:38 -0600

    Initial commit

---
+++ tune/tuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  tuneApp.swift
+//  tune
+//
+//  Created by Ivan Almada on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct tuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
