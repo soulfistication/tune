749a06ecf14ba83b76a6042c92d10958bb01fcab
Ivan Almada <ivan.almada@me.com>
2024-09-05 11:09:35 -0600

    Initial EADBGE standard guitar tuning layout

--- tune/ContentView.swift
+++ tune/ContentView.swift
@@ -10,10 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                print("E string tapped")
+            } label: {
+                Text("E")
+            }
+            Button {
+                print("A string tapped")
+            } label: {
+                Text("A")
+            }
+            Button {
+                print("D string tapped")
+            } label: {
+                Text("D")
+            }
+            Button {
+                print("G string tapped")
+            } label: {
+                Text("G")
+            }
+            Button {
+                print("B string tapped")
+            } label: {
+                Text("B")
+            }
+            Button {
+                print("Hight E string tapped")
+            } label: {
+                Text("E")
+            }
         }
         .padding()
     }
